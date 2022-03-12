9af22326f0fdf4fa309f9dd8055753adf4d5e20c
waq-e <whomomlikes@hotmail.com>
2022-03-12 00:36:25 -0600

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::vector;
+
+int main() {
+	vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+	                             {0, 1, 0, 0, 0, 0},
+	                             {0, 1, 0, 0, 0, 0},
+	                             {0, 1, 0, 0, 0, 0},
+	                             {0, 0, 0, 0, 1, 0}};
+
+	return 0;
+}
